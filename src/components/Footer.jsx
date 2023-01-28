c80a8747c0a2fc2e4667c797c9b37ad37f181680
Eden Brekke <eden.lorrai@gmail.com>
2023-01-28 12:30:28 -0800

    expanded on footer content and got it styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
-import {Typography, Link} from '@mui/material'
+import {Box, Typography, Link} from '@mui/material'
 
 const Footer = () => {
   return (
-    <Typography >
+    <Box
+      sx={{ 
+            bgcolor: 'background.paper',
+            p: 6,
+          }}
+      component='footer'
+    >
+    <Typography 
+      variant='h6'
+      align='center'
+      gutterBottom
+    >
+      Thank you for visiting my project, here are the links to my socials.
+    </Typography>
+    <Typography
+      variant='subtitle1'
+      align='center'
+      color='text.secondary'
+      component='p'
+    >
+      <Link color='inherit' href='https://github.com/eden-brekke'> GitHub </Link>
+      <Link color='inherit' href='https://www.linkedin.com/in/eden-brekke/'> LinkedIn </Link>
+    </Typography>
+    <Typography 
+      variant='body2'
+      color='text.secondary'
+      align='center'
+    >
       {'Copyright © '}
       <Link color='inherit' href='https://eden-portfolio.vercel.app/'>
         Eden Brekke
@@ -11,6 +38,7 @@ const Footer = () => {
       {new Date().getFullYear()}
       {'.'}
     </Typography>
+    </Box>
   )
 }
 

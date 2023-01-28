a6d7ff5bd7edaa497855ba470cd9913fca7e0b88
Eden Brekke <eden.lorrai@gmail.com>
2023-01-28 12:53:31 -0800

    got task list app bar up and running

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import TaskIcon from '@mui/icons-material/Task'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <AppBar position='relative'>
+      <Toolbar>
+        <TaskIcon sx={{mr:2}} />
+        <Typography>
+            Task List
+        </Typography>
+      </Toolbar>
+    </AppBar>
   )
 }
 

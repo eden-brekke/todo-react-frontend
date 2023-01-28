870365ac6f31492a2aa15885f74b7f13345819b5
Eden Brekke <eden.lorrai@gmail.com>
2023-01-28 12:03:40 -0800

    initial mui skeleton no content

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Box, Container, Typography, Stack, Button } from '@mui/material';
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <Box>
+      <Container>
+        <Typography>
+
+        </Typography>
+        <Typography>
+          
+        </Typography>
+        <Stack>
+          <Button></Button>
+          <Button></Button>
+        </Stack>
+      </Container>
+    </Box>
   )
 }
 

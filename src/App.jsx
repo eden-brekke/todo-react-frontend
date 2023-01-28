cd146c0456009f3e8d61cff97e8174027bed9a01
Eden Brekke <eden.lorrai@gmail.com>
2023-01-28 12:30:12 -0800

    added themeprovider

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import './App.css';
 import { Box } from '@mui/material';
 import Main from './components/Main';
 
+import { CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
 
 
 
 function App() {
+  const theme = createTheme();
   return (
-    <Box>
-    <Main/>
-    </Box>
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Main />
+    </ThemeProvider>
   );
 }
 

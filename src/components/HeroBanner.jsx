f955f8e03b17a030de6a9e2d5918f0d61eb79071
Eden Brekke <eden.lorrai@gmail.com>
2023-01-28 12:10:23 -0800

    added style

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -3,17 +3,41 @@ import { Box, Container, Typography, Stack, Button } from '@mui/material';
 
 const HeroBanner = () => {
   return (
-    <Box>
-      <Container>
-        <Typography>
-
+    <Box
+      sx={{
+        bgcolor: 'background.paper',
+        pt: 8,
+        pb: 6,
+      }}
+    >
+      <Container maxWidth='sm'>
+        <Typography
+          component='h1'
+          variant='h2'
+          align='center'
+          color='text.primary'
+          gutterBottom
+        >
+          Current Task List
         </Typography>
-        <Typography>
-          
+        <Typography
+          variant='h5'
+          align='center'
+          color='text.secondary'
+          paragraph
+        >
+          Here you can find your own personal task list. You can add more items, you can edit existing items, you can delete items, and you can simple strikethrough them to show you've completed them.
         </Typography>
-        <Stack>
-          <Button></Button>
-          <Button></Button>
+        <Stack
+          sx={{
+              pt:4
+            }}
+          direction='row'
+          spacing={2}
+          justifyContent='center'
+        >
+          <Button variant='contained'>Front end Source Code</Button>
+          <Button variant='outlined'>Backend Source Code</Button>
         </Stack>
       </Container>
     </Box>
